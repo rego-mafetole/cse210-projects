82af8557e69ace6a710a405d18f3de0e0bc3747f
rego-mafetole <rmafetole@gmail.com>
2026-03-06 13:55:18 +0100

    Week01 Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else if (number < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations you have passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Try again next semester.");
+        }
     }
 }
+
