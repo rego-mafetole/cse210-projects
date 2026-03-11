6f5fd9051d35f1f31a883e76e3a38ae5d1c87982
rego-mafetole <rmafetole@gmail.com>
2026-03-11 19:54:43 +0100

    Job file

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Public
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Hello World");
+    }
+
+        public class Job
+    {
+        public string _company;
+        public string _jobTitle;
+        public int _startYear;
+        public int _endYear;
+
+    }
+    }
