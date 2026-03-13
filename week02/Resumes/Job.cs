e57c6db7413020ca72627eaf031338ecc984ff66
rego-mafetole <rmafetole@gmail.com>
2026-03-13 19:58:18 +0100

    Resume exercise

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -1,18 +1,16 @@
 using System;
 
-class Public
-{
-    static void Main(string[] args)
+    public class Job 
     {
-        Console.Write("Hello World");
-    }
 
-        public class Job
-    {
-        public string _company;
         public string _jobTitle;
+        public string _company;
         public int _startYear;
         public int _endYear;
 
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_startYear}-{_endYear})");
     }
+
     }
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Capitec";
+        job2._startYear = 2017;
+        job2._endYear = 2019;
+
+        Resume myResume = new Resume();
+        myResume._personName = "Regomoditswe Mafetole";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
